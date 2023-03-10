9ef236d8fb14992a21059fe991bcc87254aa9b62
Alex Chan <alyxchan87@gmail.com>
2023-03-10 10:41:38 -0800

    switch to nightly, add rust support

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,37 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  -- Rust support
+      {
+        "simrat39/rust-tools.nvim",
+        after = { "mason-lspconfig.nvim" },
+        -- Is configured via the server_registration_override installed below!
+        config = function()
+          require("rust-tools").setup {
+            server = astronvim.lsp.server_settings "rust_analyzer",
+            tools = {
+              inlay_hints = {
+                parameter_hints_prefix = "  ",
+                other_hints_prefix = "  ",
+              },
+            },
+          }
+        end,
+      },
+      {
+        "Saecki/crates.nvim",
+        after = "nvim-cmp",
+        config = function()
+          require("crates").setup()
+          astronvim.add_cmp_source { name = "crates", priority = 1100 }
+
+          -- Crates mappings:
+          local map = vim.api.nvim_set_keymap
+          map("n", "<leader>Ct", ":lua require('crates').toggle()<cr>", { desc = "Toggle extra crates.io information" })
+          map("n", "<leader>Cr", ":lua require('crates').reload()<cr>", { desc = "Reload information from crates.io" })
+          map("n", "<leader>CU", ":lua require('crates').upgrade_crate()<cr>", { desc = "Upgrade a crate" })
+          map("v", "<leader>CU", ":lua require('crates').upgrade_crates()<cr>", { desc = "Upgrade selected crates" })
+          map("n", "<leader>CA", ":lua require('crates').upgrade_all_crates()<cr>", { desc = "Upgrade all crates" })
+        end,
+      },
 }
