6d264fe4e2e241cb84500d4413383aec954e2761
Alex Chan <alyxchan87@gmail.com>
2023-06-12 10:53:22 -0700

    Add flake8, clang-format, and debugpy mason plugins

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -23,6 +23,8 @@ return {
         "stylua",
         "black",
         "prettierd",
+        "flake8",
+        "clang-format",
       })
     end,
   },
@@ -32,7 +34,7 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        "python",
+        "debugpy",
         "codelldb",
       })
     end,
