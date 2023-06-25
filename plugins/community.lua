f5c2453ccb216a7f23c623042d085934ea9bc30d
Alexander Chan <alyxchan87@gmail.com>
2023-06-25 12:53:32 -0700

    Use astrocommunity packs, remove crates (managed by astrocommunity.pack.rust)

--- plugins/community.lua
+++ plugins/community.lua
@@ -3,7 +3,13 @@ return {
   "AstroNvim/astrocommunity",
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
+  { import = "astrocommunity.pack.astro" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.cmake" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.rust" },
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
